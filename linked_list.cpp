0ad0a6440be2908a5901f9aac0825fcd7ec3d321
Tsuzat <aloksingh772002@gmail.com>
2021-08-21 05:26:24 +0530

    Added function to reverse a linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -15,18 +15,18 @@ class Node{
 };
 
 class LinkedList {
-    Node head;
-    int len;
+    Node* head;
     public:
+        int lenght;
         // constructor
         LinkedList(Node &node){
-            head = node;
-            len = 1;
+            head = &node;
+            lenght = 1;
         }
 
         // to display linked list
         void display(){
-            Node* temp = &head;
+            Node* temp = head;
             while(temp != NULL){
                 cout<<temp->value<<" -> ";
                 temp = temp->next;
@@ -37,24 +37,69 @@ class LinkedList {
         // append
         void append(Node &node){
             // exception is when same nodes are passed multiple times
-            Node* temp = &head;
+            Node* temp = head;
             while(temp->next != NULL) temp = temp->next;
             temp->next = &node;
-            len ++;
+            lenght ++;
         }
 
         // insert in the begining of linked list
         void insert_head(Node &node){
             Node* temp = new Node();
-            temp->value = head.value;
-            temp->next = head.next;
-            head = node;
-            head.next = temp;
-            len++;
+            temp->value = head->value;
+            temp->next = head->next;
+            head = &node;
+            head->next = temp;
+            lenght++;
         }
 
-        int lenght(){
-            return len;
+        // returns true if there is a node with given value; else false
+        bool find(int val){
+            for (Node* temp = head; temp != NULL; temp = temp->next)
+                if (temp->value == val) return true;
+            return false;
+        }
+
+        // returns the pointer of a node with given value
+        Node* find_pointer(int val){
+            for (Node* temp = head; temp != NULL; temp = temp->next)
+                if (temp->value == val) return temp;
+            return NULL;
+        }
+
+        void delete_by_value(int val){
+            if (!find(val)){
+                cout<<"\nvalue is not in linked list"<<endl;
+                return;
+            }
+            Node* temp;
+            for (temp = head; temp->next != NULL; temp = temp->next)
+                if (temp->next->value == val) break;
+            temp->next = temp->next->next;
+            lenght--;
+        }
+        
+        // will throw error if there is only head is left
+        void pop(){
+            if (lenght <= 1) return;
+            Node* temp;
+            for (temp = head; temp->next->next != NULL; temp=temp->next);
+            temp->next = NULL;
+            lenght--;
+        }
+
+        void reverse(){
+            Node* previous = NULL;
+            Node* current = head;
+            Node* next_ptr;
+            while(current != NULL){
+                next_ptr = current->next;
+                current->next = previous;
+
+                previous = current;
+                current = next_ptr;
+            }
+            head = previous;
         }
 };
 
@@ -82,6 +127,17 @@ int main(){
     
     // printing LinkedList
     llist.display();
-    cout<<"\nlenght of LinkedList: "<<llist.lenght();
+    cout<<"\nlenght of LinkedList: "<<llist.lenght;
+
+    // deleting 5
+    llist.delete_by_value(5);
+    cout<<endl;
+    llist.display();
+    cout<<"\nlenght of LinkedList: "<<llist.lenght;
+
+    // reversing the linked list
+    llist.reverse();
+    cout<<"\nReversed Linked_list: ";
+    llist.display();
     return 0;
 }
